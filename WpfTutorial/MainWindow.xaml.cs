1fc9667475a43aa8fa32741cb6f68f9419de1685
Jonas Schneider <jonas.schneider@gigatronik.com>
2017-03-28 18:03:59 +0200

    Added new Image

--- WpfTutorial/MainWindow.xaml.cs
+++ WpfTutorial/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfTutorial.Support;
 
 namespace WpfTutorial
 {
@@ -20,9 +10,37 @@ namespace WpfTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int x = 0;
+        private List<PlayerIcon> playerIcons = new List<PlayerIcon>();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.hideSomething();
+            
+        }
+        private void hideSomething()
+        {
+            Console.WriteLine("wrote");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Support.PlayerIcon test = new Support.PlayerIcon();
+            test.Margin = new Thickness(10, x += 100, 10,10);
+            grid1.Children.Add(test);
+            playerIcons.Add(test);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            while (playerIcons.Count != 0)
+            {
+                grid1.Children.Remove(playerIcons[0]);
+                playerIcons.RemoveAt(0);
+            }
+            x = 0;    
         }
     }
 }
---
+++ WpfTutorial/Support/PlayerIcon.xaml.cs
@@ -0,0 +1,65 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+using System.Windows.Controls;
+
+namespace WpfTutorial.Support
+{
+    /// <summary>
+    /// Interaktionslogik für PlayerIcon.xaml
+    /// </summary>
+    public partial class PlayerIcon :  UserControl, INotifyPropertyChanged
+    {
+        public PlayerIcon()
+        {
+            DataContext = this;
+            InitializeComponent();
+            PlayerName = defaultPlayerName;
+        }
+
+        private string defaultPlayerName = "Player";
+        private string _playerName;
+
+        public string PlayerName {
+            get { return _playerName;  }
+            set
+            {
+                if (_playerName != value)
+                {
+                    _playerName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void MenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PlayerName = "TESTPLAYERNAME";
+            Console.WriteLine(PlayerName);
+        }
+
+        private void MenuItem_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PlayerName = "";
+        }
+
+        private void MenuItem_Click_2(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PlayerName = "Your POSITION";
+        }
+
+        private void MenuItem_Click_3(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PlayerName = "Yeah its donne";
+        }
+    }
+
+}
